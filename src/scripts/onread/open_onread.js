fe3e448e1a00eb28f2d6acb93f94a20bbe7da1b5
Tiago Porto <me@tiagoporto.com>
2014-12-22 02:55:25 -0200

    Unified gh-pages and master

--- src/scripts/onread/open_onread.js
+++
@@ -1,9 +0,0 @@
-/*
-	Accessibility Buttons
-	Version: 2.0.0
-	Author: Tiago Porto - http://www.tiagoporto.com
-	https://github.com/tiagoporto
-	Contact: me@tiagoporto.com
-*/
-
-$(document).ready(function(){
