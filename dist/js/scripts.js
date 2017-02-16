db2a2eb7e76228cd49cd9f422067ba44749daec9
Tiago Porto <tiago@tiagoporto.com>
2017-02-16 12:11:58 -0200

    Call when dom ready

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -6,14 +6,31 @@
 */
 
 /* global accessibilityButtons */
-accessibilityButtons({
-    font: {
-        nameButtonIncrease: 'Click me and things will be great',
-        nameButtonDecrease: 'Click me and things will turn to normal size'
-    },
 
-    contrast: {
-        nameButtonAdd: 'Click me and things will be darken',
-        nameButtonRemove: 'Click me and things will be clean again'
+var ready = function ready(fn) {
+    'use strict';
+
+    if (document.readyState !== 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
     }
+};
+
+ready(function () {
+    'use strict';
+
+    accessibilityButtons({
+        font: {
+            nameButtonIncrease: 'Click me and things will be great',
+            nameButtonDecrease: 'Click me and things will turn to normal size'
+        },
+
+        contrast: {
+            nameButtonAdd: 'Click me and things will be darken',
+            nameButtonRemove: 'Click me and things will be clean again'
+        }
+    });
+
+    document.getElementById('year').innerHTML = new Date().getFullYear();
 });
--- src/scripts/demo.js
+++ src/scripts/demo.js
@@ -1,12 +1,30 @@
 /* global accessibilityButtons */
-accessibilityButtons({
-    font: {
-        nameButtonIncrease: 'Click me and things will be great',
-        nameButtonDecrease: 'Click me and things will turn to normal size'
-    },
 
-    contrast: {
-        nameButtonAdd: 'Click me and things will be darken',
-        nameButtonRemove: 'Click me and things will be clean again'
+const ready = fn => {
+    'use strict';
+
+    if (document.readyState !== 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
     }
+};
+
+ready(() => {
+    'use strict';
+
+    accessibilityButtons({
+        font: {
+            nameButtonIncrease: 'Click me and things will be great',
+            nameButtonDecrease: 'Click me and things will turn to normal size'
+        },
+
+        contrast: {
+            nameButtonAdd: 'Click me and things will be darken',
+            nameButtonRemove: 'Click me and things will be clean again'
+        }
+    });
+
+
+    document.getElementById('year').innerHTML = new Date().getFullYear();
 });
