122144f3642cc4cdeb473c7b7c013025e8d593b6
Tiago Porto <tiago@tiagoporto.com>
2017-05-13 22:09:08 +1200

    Add analytics events

--- src/scripts/demo.js
+++ src/scripts/demo.js
@@ -27,4 +27,37 @@ ready(() => {
 
 
     document.getElementById('year').innerHTML = new Date().getFullYear();
+
+    function hasClass(element, clazz) {
+        return (` ${element.className} `).indexOf(` ${clazz} `) > -1;
+    }
+
+    function analytics() {
+        return function() {
+            var $this = this;
+            var $body = document.body;
+
+            if ($this.getAttribute('id') === 'accessibility-contrast') {
+                if (hasClass($body, $this.getAttribute('id'))) {
+                    typeof ga === 'function' && ga('send', 'event', 'accessibility', 'click', 'Add Contrast');
+                } else {
+                    typeof ga === 'function' && ga('send', 'event', 'accessibility', 'click', 'Remove Contrast');
+                }
+            }
+
+            if ($this.getAttribute('id') === 'accessibility-font') {
+                if (hasClass($body, $this.getAttribute('id'))) {
+                    typeof ga === 'function' && ga('send', 'event', 'accessibility', 'click', 'Increase Font');
+                } else {
+                    typeof ga === 'function' && ga('send', 'event', 'accessibility', 'click', 'Decrease Font');
+                }
+            }
+        };
+    }
+
+    var $accessibilityButtons = document.getElementsByClassName('js-acessibility');
+
+    for (var i = 0; i < $accessibilityButtons.length; i++) {
+        $accessibilityButtons[i].addEventListener('click', analytics());
+    }
 });
