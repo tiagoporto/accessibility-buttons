a86003acd13c4f3caaf74ef9520351cb86f8b38a
Tiago Porto <tiagoporto@ymail.com>
2018-10-21 13:40:42 -0300

    Update button styles

--- dist/js/vendors.js
+++ dist/js/vendors.js
@@ -16,17 +16,6 @@ addLoadEvent(
     $('#logo').downloadTip({ 'position': 'bottom' })
 );
 
-/* eslint-disable */
-
-// Google Analytics: change UA-XXXXX-X to be your site's ID.
-(function(i,s,o,g,r,a,m){i.GoogleAnalyticsObject=r;i[r]=i[r]
-||function(){(i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-32351360-4', 'auto');
-ga('send', 'pageview');
-
 // This is the "Offline copy of pages" service worker
 
 // Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
