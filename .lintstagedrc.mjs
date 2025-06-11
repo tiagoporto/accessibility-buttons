815760f05a2246db9f83a374070f25f9ad38a442
Tiago Porto <tiago@tiagoporto.com>
2025-06-10 23:33:57 -0300

    ci: add typecheck

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -2,7 +2,7 @@ export default {
   '*': 'prettier --check --ignore-unknown --write',
   '*.{md,markdown,mdx}': ['remark --frail', 'eslint --max-warnings 0'],
   '*.{scss,css}': 'stylelint',
-  // '*.{ts,tsx}': () => 'tsc --project tsconfig.json',
+  '*.{ts,tsx}': () => 'tsc --project tsconfig.json',
   '*.{html,yml,json,jsonc,json5}': 'eslint --max-warnings 0 --no-warn-ignored',
   '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': [
     'eslint --max-warnings 0 --no-warn-ignored',
