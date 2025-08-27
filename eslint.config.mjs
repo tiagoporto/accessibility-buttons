1ec70756b65e6c21396c705824512d5c665f4ad1
Tiago Porto <tiago@tiagoporto.com>
2025-08-27 14:56:05 -0300

    chore: eslint ignore test folders

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import globals from 'globals'
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: ['**/dist/'],
+    ignores: ['**/dist/', '**/coverage/**', 'playwright-report/'],
   },
   {
     languageOptions: {
